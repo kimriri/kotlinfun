6aeaf8c5a8c7112783f34c945832a8ae691320ff
kimriri <dmsal547@gmail.com>
2022-07-11 21:50:37 +0900

    CoroutineScope async

---
+++ app/src/main/java/com/programmers/kotlinfun/Coroutine.kt
@@ -0,0 +1,34 @@
+package com.programmers.kotlinfun
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.ui.AppBarConfiguration
+import com.programmers.kotlinfun.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class Coroutine : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+
+        CoroutineScope(Dispatchers.Default).async {
+
+            val deferred = async {
+                delay(5000)
+                2500
+            }
+            val deferred2 = async {
+                delay(500)
+                100
+            }
+            Log.d("CoroutineScope"
+                ,"${deferred.await() + deferred2.await()}")
+        }
+
+
+    }
+}
